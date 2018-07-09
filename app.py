e45159be4325a9dedeccea39e2fbe691e380ba9a
aravind <aravind.prasanna@gmail.com>
2018-07-09 15:43:40 +0800

    Made changes to accomodate the offers fetch part

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ url_domain = "http://d7dafd75.ngrok.io/"
 
 #canned responses
 FOUND_X_OFFERS = "I found {} offers. Would you like me to refine the search?"
+FOUND_SPEC_OFFER = ''' Here is the offer. {} . You can get the next offer by saying "Next". 
+'''
 
 
 @app.route('/webhook',methods=['POST'])
@@ -36,11 +38,44 @@ def webhook():
 
 def get_offer(req_json):
     session = req_json.get("session")
-    offer_type = req_json["queryResult"]["parameters"]["offer_type"]
-    api_name = "get/offers/{0}/all/all".format(offer_type)
-    response_json = call_offers_voice(url_domain + api_name)
+    print(req_json)
+    output_contexts = req_json["queryResult"].get("outputContexts")
+    print("output", output_contexts)
+    context_name = session + "/contexts/offer_context"
+    offers_list = []
+    offer_index = 0
+    offer_type = ""
+    for context_item in output_contexts:
+        if context_item.get("name") == context_name:
+            offers_list = context_item["parameters"]["offer_list"]
+            offer_index = context_item["parameters"]["offer_index"]
+            offer_type = context_item["parameters"]["offer_type"]
 
-    return response_json
+    selected_offer = offers_list[offer_index+1]
+
+    api_name = "get/offer/{0}/".format(selected_offer)
+    resp_json = call_offers_voice(url_domain + api_name)
+
+    offer_details = resp_json["offer_details"]
+
+    context_lifespan = 5
+    offer_index += 1
+    offer_activities = ""
+    offer_card = ""
+
+    #offer_details = selected_offer["offer_details"]
+    print(offer_details)
+    speech = FOUND_SPEC_OFFER.format(offer_details)
+    resp = build_response_json(speech,
+                                        context_name,
+                                        context_lifespan,
+                                        offer_type,
+                                        offers_list,
+                                        offer_index,
+                                        offer_activities,
+                                        offer_card)
+
+    return resp
 
 def get_offers(req_json):
     session = req_json.get("session")
